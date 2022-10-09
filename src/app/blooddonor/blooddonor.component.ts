0d27b0e27fc265565bd3e9d7b79d0a66136aca0c
Ann-Annice <annannice98@gmail.com>
2022-10-09 20:35:09 +0530

    adddonor

---
+++ src/app/blooddonor/blooddonor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blooddonor',
+  templateUrl: './blooddonor.component.html',
+  styleUrls: ['./blooddonor.component.css']
+})
+export class BlooddonorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
