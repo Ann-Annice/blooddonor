482f0cc95ba9303ae80cd8c6c4b75fab770f73d9
Ann-Annice <annannice98@gmail.com>
2022-10-10 10:02:28 +0530

    view

---
+++ src/app/viewalldonor/viewalldonor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewalldonor',
+  templateUrl: './viewalldonor.component.html',
+  styleUrls: ['./viewalldonor.component.css']
+})
+export class ViewalldonorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
